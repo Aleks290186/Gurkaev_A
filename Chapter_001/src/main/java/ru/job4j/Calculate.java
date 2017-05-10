87fd75cbcee978915a66b3eff593990e518e76f8
Aleksey Gurkaev <aleks290186@yandex.ru>
2017-05-10 18:00:35 +0300

    add javadoc

--- Chapter_001/src/main/java/ru/job4j/Calculate.java
+++ Chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+* My first program on JAVA!!!!
+* @author Gurkaev_A
+* @since 10.05.2016
+*/
+
 public class Calculate {
+	/**
+	*Just main method
+	*/
+	
    public static void main(String[] args){
 	System.out.println("Hello World!");
   }
---
+++ Chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Gurkaev Aleksey
+* @version 0.1
+* @since 10.05.2017
+*/
+package ru.job4j;
